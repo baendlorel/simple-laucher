538e89112139ead306af224142c70097893e72ee
aldia_mechdream <futami16237@gmail.com>
2026-05-25 16:52:32 +0800

    feat: 添加对Cargo.toml的支持，更新加载命令的逻辑

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,75 @@
 import type { CommandConfig, LoadFrom } from '@/types/index.js';
 import vscode from 'vscode';
+import { parse } from 'smol-toml';
 
-export const config = () => vscode.workspace.getConfiguration('simple-launcher');
+const config = () => vscode.workspace.getConfiguration('simple-launcher');
 
-export const load = () => {
-  const loadFrom = new Set(config().get<LoadFrom[]>('load-from', []));
-  const custom = config().get<CommandConfig[]>('custom-commands', []);
+const loadFromJson = async (root: vscode.WorkspaceFolder | undefined) => {
+  if (!root) {
+    return [];
+  }
+
+  const content = await vscode.workspace.fs.readFile(vscode.Uri.joinPath(root.uri, 'package.json'));
+  const data = JSON.parse(content.toString()) as { scripts?: Record<string, string> };
+  if (!data.scripts) {
+    return [];
+  }
+
+  return Object.entries(data.scripts).map(
+    ([key, value]): CommandConfig => ({
+      displayName: key,
+      command: value,
+      from: 'package.json',
+    }),
+  );
+};
+
+const loadFromCargoToml = async (root: vscode.WorkspaceFolder | undefined) => {
+  if (!root) {
+    return [];
+  }
+
+  const content = await vscode.workspace.fs.readFile(vscode.Uri.joinPath(root.uri, 'Cargo.toml'));
+  const data = parse(content.toString()) as { package: { name: string }; workspace?: { members?: string[] } };
+  const members = data.workspace?.members;
+  if (!Array.isArray(members)) {
+    // Only root command
+    return [
+      {
+        displayName: data.package.name,
+        command: `cargo run`,
+        from: 'Cargo.toml',
+      } satisfies CommandConfig,
+    ];
+  }
+
+  const cargoTomlPaths = members.map((member) => vscode.Uri.joinPath(root.uri, member, 'Cargo.toml'));
+  const cargoTomlContents = await Promise.all(
+    cargoTomlPaths.map((path) => vscode.workspace.fs.readFile(path).then((content) => content.toString())),
+  );
+
+  return cargoTomlContents
+    .map((content) => parse(content) as { package?: { name?: string } })
+    .filter((data): data is { package: { name: string } } => Boolean(data.package?.name))
+    .map(
+      (data): CommandConfig => ({
+        displayName: data.package!.name,
+        command: `cargo run --bin ${data.package!.name}`,
+        from: 'Cargo.toml',
+      }),
+    );
+};
+
+export const load = async () => {
+  const loadFrom = config().get<LoadFrom[]>('load-from', []);
+  const commands = config().get<CommandConfig[]>('custom-commands', []);
+  if (loadFrom.includes('package.json')) {
+    const arr = await loadFromJson(vscode.workspace.workspaceFolders?.[0]);
+    commands.push(...arr);
+  }
+
+  if (loadFrom.includes('Cargo.toml')) {
+  }
 };
 
 export const save = (commands: CommandConfig[], configurationTarget = vscode.ConfigurationTarget.Workspace) => {
